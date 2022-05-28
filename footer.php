4a1c3c95ca317e0defd444f28b7522fdbee469a0
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-05-28 19:23:25 -0300

    script movido para arquivo separado

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<header>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="./src/assets/js/main.js"></script>
+    <style>
+        
+    .footer {
+    position: absolute;
+    bottom: 1;  
+    width: 100%;
+    background-color: #f0f8ff; !important
+    /*text-align: center;
+    line-height: 100px;*/
+    }
+    </style>
+</header>
+<body>
+<div class="footer">
+    <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3">
+            © 2022 Copyright:
+            <a class="text-dark" href=""></a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+</div>
+</body>
+</html>
---
+++ src/assets/js/main.js
@@ -0,0 +1,55 @@
+// chamada da api de CEP
+function formulário_cep() {
+    // Limpa valores do formulário de cep.
+    $("#rua").val("");
+    $("#bairro").val("");
+    $("#cidade").val("");
+    $("#uf").val("");
+    $("#ibge").val("");
+}
+
+//Quando o campo cep perde o foco.
+$("#cep").blur(function () {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = $(this).val().replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            $("#rua").val("...");
+            $("#bairro").val("...");
+            $("#cidade").val("...");
+            $("#uf").val("...");
+
+            //Consulta o webservice viacep.com.br/
+            $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dadosRetorno) {
+
+                if (!("erro" in dadosRetorno)) {
+                    //Atualiza os campos com os valores da consulta.
+                    $("#rua").val(dadosRetorno.logradouro);
+                    $("#bairro").val(dadosRetorno.bairro);
+                    $("#cidade").val(dadosRetorno.localidade);
+                    $("#uf").val(dadosRetorno.uf);
+                } //end if.
+                else {
+                    //CEP pesquisado não foi encontrado.
+                    $(this)[0].reset();
+                    alert("CEP não encontrado.");
+                }
+            });
+        } //end if.
+        else {
+            //cep é inválido.
+            $(this)[0].reset();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+});
