c50feef3012c87ece541c9bb813e2d3063a3e84e
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-05-28 19:50:19 -0300

    refeita a formação

--- footer.php
+++ footer.php
@@ -5,20 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="./src/assets/js/main.js"></script>
-    <style>
-        
-    .footer {
-    position: absolute;
-    bottom: 1;  
-    width: 100%;
-    }
-    </style>
 </header>
 <body>
-<div class="footer">
-    <footer class="bg-light text-center text-lg-start">
+<div class="absolute bottom-px- w-full inline-block">
+    <footer class="bg-light text-center text-lg-start bottom-px">
         <!-- Copyright -->
-        <div class="text-center p-3">
+        <div class="text-center p-3 bottom-px">
             © 2022 Copyright:
             <a class="text-dark" href=""></a>
         </div>
