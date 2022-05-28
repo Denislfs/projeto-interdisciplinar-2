b69a684c0dd12eb4cce41757e86065196bb98712
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-05-28 19:51:25 -0300

    Refeita formatação da header

--- header.php
+++ header.php
@@ -4,51 +4,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Importando Script bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <!--Importando Script tailwindcss-->
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Style -->
+    <link rel="stylesheet" href="./src/assets/style/home.css" />
+    <!--Importando Script-->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <!--Importando main js -->
+    <script language="JavaScript" href="./src/assets/js/main.js"></script>
     <!--Importando Script Jquery-->
-	  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
 </head>
 <body>
   <!--Navbar-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="cadastroentreg.php" role="button" data-bs-toggle="dropdown">Cadastros</a>
-          <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="cadastroentreg.php">Entregadores</a></li>
-              <li><a class="dropdown-item" href="cadastroest.php">Estabelecimentos</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="atualiza.php">Atualizar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="deletar.php">Deletar</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+  <nav class="flex items-center place-content-around rounded-md">
+            <div>
+                <h3>LOGO</h3>
+            </div>
+            <div>
+                <ul class="flex items-center justify-center gap-x-16 py-12 text-gray-500">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="flex items-center justify-center">
+                        <a href="#">Serviços</a>
+                        <div x-data="{ dropdownOpen: false }" class="relative">
+                            <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block rounded-md bg-white p-2 focus:outline-none">
+                                <svg class="h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" 
+                                    />
+                                </svg>
+                            </button>
+                            
+                            <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+                            <div x-show="dropdownOpen"
+                                class="absolute right-0 mt-1 py-2 w-48 bg-white rounded-md shadow-lg z-20 border border-gray-100">
+                                <a href="cadastroentreg.php"
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-500 hover:text-white">
+                                    Seja um entregador
+                                </a>
+                                <a href="cadastroest.php"
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-500 hover:text-white">
+                                    Seja uma empresa parceira
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">Quem somos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 </body>
 </html>
