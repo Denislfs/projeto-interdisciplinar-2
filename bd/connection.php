d28a9ed315f53e4704f35c09d196e4229bb93233
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-06-10 21:20:06 -0300

    Criada uma classe com os dados do bando de dados e funções para conexão e destruição

---
+++ bd/connection.php
@@ -0,0 +1,23 @@
+<?php
+
+class DataBaseService {
+    public $servername = 'localhost';
+    public $username = 'root';
+    public $password = '';
+    public $dbname = 'bikelive';
+
+    public function __construct()
+    {
+        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname);
+
+        if(!$this->conn) {
+            die("Falha na conexão: " . mysqli_connect_error());
+        }
+    }
+
+    public function __destruct()
+    {
+        mysqli_close($this->conn);
+    }
+}
+?>
