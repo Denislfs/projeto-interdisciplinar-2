46a0319b033ad7f5ed0d0b758ec3d6d3c6971303
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-06-10 21:19:03 -0300

    Chamando conexão do banco de dados e dados do banco com herança

--- delete/deletarpedido2.php
+++ delete/deletarpedido2.php
@@ -1,26 +1,9 @@
 <?php
 
-class DataBaseService {
-    public $servername = 'localhost';
-    public $username = 'root';
-    public $password = '';
-    public $dbname = 'bikelive';
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once (__ROOT__.'./bd/connection.php');
 
-    //função para conexão
-    public function __construct()
-    {
-        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname);
-
-        if(!$this->conn) {
-            die("Falha na conexão: " . mysqli_connect_error());
-        }
-    }
-
-    //função para destruir conexão
-    public function __destruct()
-    {
-        mysqli_close($this->conn);
-    }
+class DataBase extends DataBaseService{
 
     public function deletarEstabelecimento($id) {
         
@@ -49,7 +32,7 @@ class DataBaseService {
 
     if(!empty($_POST)) {
         $id = $_POST['id'];
-        $deletarCadastro = new DataBaseService();
+        $deletarCadastro = new DataBase();
         $deletarCadastro -> deletarEstabelecimento($id);
     };
     
